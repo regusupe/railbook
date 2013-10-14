b849edf6a96757d35849bbdded4b57a41db766fa
regusupe <regusupe@Kazumasa-no-MacBook-Air.local>
2013-10-14 18:37:45 +0900

    RoR3の３章まで完了

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railbook::Application.config.secret_token = '0ba683ebaf91daf9a8c51fef7bed01e9b6dd9526e937aa44bc05511abf69eea96123e19e6c399e47b45bd1c2ad910824fbb4a6f1a5c4a157f8d9d4ea957b8165'
